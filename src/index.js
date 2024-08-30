645d50041f90bf1b3384eea29942835f30826072
Israel Quisbert <israelquisbert.dev@gmail.com>
2024-08-29 22:48:09 -0400

    Rollback

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { HashRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter router={router} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
